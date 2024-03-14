d8c68c58790f5a8f13a562229afb8b5d823485ec
Mugdha Rayanade <mugdha.rayanade@t-systems.com>
2024-03-14 13:06:09 +0530

    Initial Project setup with gitignore file

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by A115716806 on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
